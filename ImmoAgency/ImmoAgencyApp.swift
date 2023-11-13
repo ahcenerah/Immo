ba102cf8dffdac98208ec163ff694de60b7e4233
Ahcene Rahab <ahcenerahab2@gmail.com>
2023-11-13 11:19:24 +0100

    Initial Commit

---
+++ ImmoAgency/ImmoAgencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmoAgencyApp.swift
+//  ImmoAgency
+//
+//  Created by rahab ahcene on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImmoAgencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
